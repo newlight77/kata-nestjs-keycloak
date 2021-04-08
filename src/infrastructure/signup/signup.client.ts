b79483b974103dd82f1640a62ac4aa6aeec80eaf
Kong To <newlight77@gmail.com>
2021-04-08 23:05:02 +0200

    loading .env and keycloak not workling with token

--- src/infrastructure/signup/signup.client.ts
+++ src/infrastructure/signup/signup.client.ts
@@ -1,16 +1,22 @@
-import { HttpService, Injectable } from '@nestjs/common';
+import { HttpService, Inject, Injectable } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
 import { AxiosResponse } from 'axios';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import coreConfig from 'src/environment/core.config';
 
 @Injectable()
 export class SignupClient {
-  private readonly SIGNUP_URL = 'http://localhost:8080/api/signup';
-
-  constructor(private readonly http: HttpService) {}
+  constructor(
+    @Inject(coreConfig.KEY)
+    private config: ConfigType<typeof coreConfig>,
+    private readonly http: HttpService,
+  ) {
+    console.log(`coreApiUrl = ${this.config.coreApiUrl}`);
+  }
 
   findAll(): Observable<Array<SignupDomain>> {
-    return this.http.get(this.SIGNUP_URL).pipe(
+    return this.http.get(this.config.coreApiUrl).pipe(
       map((axiosResponse: AxiosResponse) => {
         return axiosResponse.data;
       }),
