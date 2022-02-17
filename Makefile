86d218a825c08c4dd4a901dc3e6f9b075afed596
Kong To <newlight77@gmail.com>
2022-02-17 16:27:47 +0100

    test: adding makefile and test-api.sh

---
+++ Makefile
@@ -0,0 +1,67 @@
+#!make
+
+# Makefile for Demo Auth Serve
+SHELL := /bin/sh
+
+
+export ENV ?= local
+export ENABLE_BUILD?= true
+
+#export BUILD = $(shell git describe --always)-$(shell date +%Y%m%d%H%M%S)
+export TAG = $(shell git describe --abbrev=0 --tags)
+#BRANCH = $(shell git branch --show-current)
+export VERSION ?= $(shell git describe --always)
+export SHORT_VERSION ?= $(git describe --always | cut -d'-' -f1)
+
+$(info environment = $(ENV))
+$(info version = $(VERSION))
+$(info enable build = $(ENABLE_BUILD))
+
+env:
+	@env
+
+help: ## Display this help screen
+	@grep -h -E '^[a-zA-Z0-9_-]+:.*?## .*$$' $(MAKEFILE_LIST) | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-20s\033[0m %s\n", $$1, $$2}'
+
+clean: dc-clean
+
+dc-build:
+	@docker system prune -f
+	@docker-compose build backend
+
+dc-build-push:
+	@docker system prune -f
+	@docker-compose build backend
+	@docker-compose push backend
+
+dc-push:
+	@docker-compose push backend
+
+dc-pull:
+	@docker-compose pull
+
+dc-up:
+	@docker-compose up -d
+
+dc-up-latest:
+	@VERSION=latest && docker-compose up -d
+
+dc-down:
+	@docker-compose down
+
+dc-down-clean:
+	@docker-compose down
+	@docker volume prune
+	@docker network prune
+
+dc-clean:
+	@docker system prune
+	@docker volume prune
+	@docker network prune
+
+dc-restart: down up
+
+
+# make test-api testId=test1
+test-api-local:
+	@./test-api.sh --api-url=http://localhost:5000/api --client-id=local.frontend.http.localhost-4200 --token-url=http://localhost:1080/auth/realms/local.app/protocol/openid-connect/token --username=newlight77+${testId}@gmail.com
---
+++ test-api.sh
@@ -0,0 +1,135 @@
+#!/bin/sh
+
+#############################################
+## Global variables
+#############################################
+if [ -x /usr/bin/tput ]; then
+  red=`tput setaf 1` # error
+  green=`tput setaf 2` # nice
+  yellow=`tput setaf 3` # warning
+  blue=`tput setaf 4` # info
+  purple=`tput setaf 5` # command
+  teal=`tput setaf 6` # detail
+  white=`tput setaf 7` #
+  reset=`tput sgr0`
+fi
+
+API_URL=http://localhost:5000/api
+CLIENT_ID=local.frontend.https
+USERNAME=newlight77+test1@gmail.com
+PASSWORD=backend1
+TOKEN_URL=http://localhost:1080/auth/realms/local.app/protocol/openid-connect/token
+TOKEN=
+
+#############################################
+## Functions
+#############################################
+logInfo() {
+  echo ${reset} $@ ${reset}  2>&1 | tee -a ${LOG_FILE}
+}
+
+logError() {
+  echo ${red} $@ ${reset}  2>&1 | tee -a ${LOG_FILE}
+}
+
+logWarning() {
+  echo ${yellow} $@ ${reset}  2>&1 | tee -a ${LOG_FILE}
+}
+
+logCmd() {
+  echo ${green} $@ ${reset}  2>&1 | tee -a ${LOG_FILE}
+}
+
+usage() {
+  echo "Usage: $0 [options]"
+  echo ""
+  echo "${blue}Options:    ${reset}"
+  echo "${blue}          -c, --classpath =[classpath]                           classpath${reset}"
+  echo "${blue}          -k, --keycloak-configFile =[keycloak.json file path]   keycloak.json file path${reset}"
+  echo "${blue}          -c, --token-url =[keycloak-token-url]                  url${reset}"
+  echo "${blue}          -h,  --help                                            help ${reset}"
+  echo "${blue}                                                                 ${reset}"
+  echo "${blue}By default, this will run with --spring.profiles.active=test ${reset}"
+  exit 1
+}
+
+signup() {
+  curl --insecure --location --request POST "${API_URL}/signup" \
+    --header 'Content-Type: application/json' \
+    --data-raw '{
+        "firstname": "kong", 
+        "lastname": "to", 
+        "username": "'${USERNAME}'",
+        "password": "'${PASSWORD}'",
+        "phoneNumber": "0659401130"
+    }'
+    echo ""
+}
+
+getToken() {
+  curl --location --request POST "${TOKEN_URL}" \
+    --header 'Content-Type: application/x-www-form-urlencoded' \
+    --data-urlencode "client_id=${CLIENT_ID}" \
+    --data-urlencode "username=${USERNAME}" \
+    --data-urlencode "password=${PASSWORD}" \
+    --data-urlencode "scope=openid" \
+    --data-urlencode "grant_type=password" | jq -r .access_token
+}
+
+unsecureApi() {
+  curl --location --request GET "${API_URL}/unsecure"
+}
+
+secureApi() {
+  token=$1
+  curl --location --request GET "${API_URL}/secure" \
+    -H "Authorization: Bearer ${token}"
+}
+
+
+#############################################
+## Check arguments
+#############################################
+for i in "$@"
+  do
+    case $i in
+      -a=*|--api-url=*)                API_URL="${i#*=}"   ;;
+      -c=*|--client-id=*)              CLIENT_ID="${i#*=}"   ;;
+      -u=*|--username=*)               USERNAME="${i#*=}"   ;;
+      -p=*|--password=*)               PASSWORD="${i#*=}"   ;;
+      -t=*|--token-url=*)              TOKEN_URL="${i#*=}"   ;;
+      -h|--help)                       usage               ;;
+      *)                               usage               ;;
+    esac
+done
+
+#############################################
+## Run
+#############################################
+
+echo "call helloApi..."
+helloApi
+echo ""
+
+#echo "signing up"
+#signup
+
+echo "call getToken..."
+TOKEN=$(getToken)
+echo $TOKEN
+echo ""
+
+echo "call secureUserApi..."
+result=$(secureUserApi ${TOKEN})
+
+echo $result
+
+if [ "$result" != "" ]; then 
+  echo $result
+else
+  echo "can't get the signup"
+  echo "maybe the issue is not valid. "
+  echo "notice : inside the container, the issuer is http://docker_keycloak_1:8080/auth"
+  echo "         and the is script is trying with issuer is http://localhost:1080/auth"
+fi
+
