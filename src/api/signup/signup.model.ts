c87b37d8716a67b17dc610728694e9a67fe73e56
Kong To <newlight77@gmail.com>
2022-03-06 20:20:16 +0100

    DOC: Feature signup: adding signup api documentation (swagger)

--- src/api/signup/signup.model.ts
+++ src/api/signup/signup.model.ts
@@ -1,8 +1,17 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsUUID } from 'class-validator';
+
 class SignupModel {
+  @ApiProperty({ required: true })
+  @IsUUID()
   username = '';
+  @ApiProperty({ required: true })
   password = '';
+  @ApiProperty({ required: true })
   firstname = '';
+  @ApiProperty({ required: true })
   lastname = '';
+  @ApiProperty({ required: true })
   phoneNumber = '';
   status = '';
 }
