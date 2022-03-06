7e9e5c25d723681d3bc50ee23aeeb83c06a9e424
Kong To <newlight77@gmail.com>
2022-03-06 20:20:16 +0100

    DOC: Feature signup: adding signup api documentation (swagger)

--- src/api/signup/signup.controller.ts
+++ src/api/signup/signup.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Post } from '@nestjs/common';
 import { SignupService } from '../../domain/signup/signup.service';
 
 import { Roles, Scopes, Public, RoleMatchingMode } from 'nest-keycloak-connect';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('signup')
 export class SignupController {
@@ -9,6 +10,8 @@ export class SignupController {
 
   @Get()
   @Public()
+  @ApiOperation({ summary: 'Fin all signups' })
+  @ApiResponse({ status: 200, description: 'Singups found' })
   async findAll() {
     return await this.appService.findAll();
   }
@@ -16,13 +19,17 @@ export class SignupController {
   @Roles({ roles: ['admin', 'realm:sysadmin'], mode: RoleMatchingMode.ALL })
   @Scopes('edit', 'create')
   @Post()
+  @ApiOperation({ summary: 'New signup' })
+  @ApiResponse({ status: 201, description: 'Successfully siggned up' })
   create(): string {
     return this.appService.create();
   }
 
   @Roles({ roles: ['user', 'other'] })
   @Scopes('view')
-  @Get()
+  @Get(':id')
+  @ApiOperation({ summary: 'Find a signup by username' })
+  @ApiResponse({ status: 201, description: 'Successfully siggned up' })
   async find() {
     console.info('api find all');
     return await this.appService.find();
