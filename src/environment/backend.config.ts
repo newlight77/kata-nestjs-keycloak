2eb0208c98ce85ebbb21099ca667e025b3375540
Kong To <newlight77@gmail.com>
2022-03-13 00:38:55 +0100

    TEST: BUILD: AUTH: properly read from keycloak.env when running local

--- src/environment/backend.config.ts
+++ src/environment/backend.config.ts
@@ -1,4 +1,9 @@
 import { registerAs } from '@nestjs/config';
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: __dirname + '/../../config/backend.env' });
+
+console.log(`SIGNUP_API_URL : ${process.env.SIGNUP_API_URL}`);
 
 export default registerAs('backendApi', () => ({
   signupApiUrl: process.env.SIGNUP_API_URL,
--- src/environment/keycloak.config.ts
+++ src/environment/keycloak.config.ts
@@ -1,4 +1,12 @@
 import { registerAs } from '@nestjs/config';
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: __dirname + '/../../config/keycloak.env' });
+
+console.log(`AUTH_SERVER_URL : ${process.env.AUTH_SERVER_URL}`);
+console.log(`REALM : ${process.env.REALM}`);
+console.log(`CLIENT_ID : ${process.env.CLIENT_ID}`);
+console.log(`CLIENT_SECRET : ${process.env.CLIENT_SECRET}`);
 
 export default registerAs('keycloak', () => ({
   authServerUrl: process.env.AUTH_SERVER_URL,
