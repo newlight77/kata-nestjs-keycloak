a9743776a7a0a09ed2468a937fac73f8fd552708
Kong To <newlight77@gmail.com>
2022-04-08 21:30:51 +0200

    TEST: FEATURE job: passing job editing test on cucumber at interface level

--- test/cucumber/interface/job.interface.step.ts
+++ test/cucumber/interface/job.interface.step.ts
@@ -11,6 +11,7 @@ import { JobComponentModule } from '../../../src/modules/job.component.module';
 import backendConfig from '../../../src/environment/backend.config';
 import { TestDatabaseModule } from '../../modules/test.database.module';
 import { getConnection } from 'typeorm';
+import { fromDomain } from '../../../src/infrastructure/job/job.entity';
 
 Before(async function () {
   const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -26,10 +27,11 @@ Before(async function () {
 
   this.app = moduleFixture.createNestApplication();
   await this.app.init();
+
+  this.dbConnect = getConnection('test-default');
 });
 
 After(async function () {
-  this.dbConnect = getConnection('test-default');
   const entities = this.dbConnect.entityMetadatas;
 
   for (const entity of entities) {
@@ -66,4 +68,35 @@ Then('a message <message> is shown', function (dataTable) {
   const expectedMessage = dataTable.rowsHash()['message'];
   const message = this.result['message'];
   expect(message).to.eql(expectedMessage);
+});
+
+Given('an existing job with details as followed', function (dataTable) {
+  this.job = dataTable.rowsHash();
+  const entity = fromDomain(this.job);
+  entity.created_at = new Date();
+  entity.updated_at = new Date();
+  const repository = this.dbConnect.getRepository(entity.constructor.name);
+  repository.save(entity);
+});
+
+When(
+  'The user updates a few attributes of the job identified by id as shown',
+  async function (dataTable) {
+    this.partialJob = dataTable.rowsHash();
+    await request(this.app.getHttpServer())
+      .put('/jobs/' + this.partialJob.id)
+      .send(this.partialJob)
+      .expect(HttpStatus.OK)
+      .then((res) => {
+        this.result = res.body;
+      });
+  },
+);
+
+Then('The job is modified as followed', function (dataTable) {
+  this.expectedJob = dataTable.rowsHash();
+  expect(this.result.job.title).to.eql(this.expectedJob.title);
+  expect(this.result.job.company).to.eql(this.expectedJob.company);
+  expect(this.result.job.salary).to.eql(this.expectedJob.salary);
+  expect(this.result.job.description).to.eql(this.expectedJob.description);
 });
